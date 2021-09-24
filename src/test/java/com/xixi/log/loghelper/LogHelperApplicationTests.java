8ddfcc42944ad64e7f85308cde5f64ca73983dd9
shengchengchao <shengchengchao@ewell.cc>
2021-09-24 15:45:05 +0800

    初始化

---
+++ src/test/java/com/xixi/log/loghelper/LogHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xixi.log.loghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
